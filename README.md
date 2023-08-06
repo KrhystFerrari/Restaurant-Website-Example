76f19d0146497ae8a2412aae3799e216342bd940
Krhystofferson Ferrari <102234632+KrhystFerrari@users.noreply.github.com>
2023-08-06 18:31:48 -0300

    Project is Done!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reactjs Material UI Resturant website
+
+## Complete Reactjs Resturant Website Example
+
+### By Krhyst Ferrari
---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Pasta from "../images/pasta.jpg";
+import Pizza from "../images/pizza.jpg";
+import Paeja from "../images/paeja.jpg";
+import Steak from "../images/steak.jpg";
+import FishFood from "../images/fish.jpg";
+import Burger from "../images/burger.jpg";
+
+export const MenuList = [
+  {
+    name: "Pasta",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Pasta,
+    price: 200,
+  },
+  {
+    name: "Pizza",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Pizza,
+    price: 250,
+  },
+  {
+    name: "Paeja",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paeja,
+    price: 300,
+  },
+  {
+    name: "Steak",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Steak,
+    price: 100,
+  },
+  {
+    name: "Fish Food",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: FishFood,
+    price: 400,
+  },
+  {
+    name: "Burger",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Burger,
+    price: 500,
+  },
+];
